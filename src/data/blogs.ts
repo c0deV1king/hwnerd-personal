25fbb2f282399970c06656b9c2ab1dea446585ec
Austin <austinjhiggins@pm.me>
2025-09-24 22:00:57 -0230

    Added another blog spot and some placeholder models

--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -7,4 +7,10 @@ export const blogs: Blog[] = [
     date: "08-02-2025",
     slug: "project-rocket-explorer",
   },
+  {
+    title: "goofy pdfs: project pitch",
+    tags: "project, electron, react, javascript",
+    date: "09-24-2025",
+    slug: "project-pitch-goofypdfs",
+  },
 ];
---
+++ src/pages/blog/project-pitch-goofypdfs.md
@@ -0,0 +1,8 @@
+---
+layout: ../../components/blogLayouts/BlogLayout.astro
+title: "goofy pdfs: project pitch"
+date: "09-24-2025"
+tags: ["project", "electron", "react", "javascript"]
+---
+
+hi test
