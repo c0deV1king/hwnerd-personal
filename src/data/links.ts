c4fa02f28c25fbebac0179ce3b5b4c4bceee0c89
austin <austinjhiggins@pm.me>
2026-06-18 16:00:35 -0230

    added links to test

--- src/data/links.ts
+++ src/data/links.ts
@@ -4,5 +4,13 @@ export const links: Link[] = [
   {
     title: "test title",
     link: "https://example.com/test"
+  },
+  {
+    title: "windows bloat tools and ai remover",
+    link: "https://irosyadi.github.io/app/windows-debloat-tool.html"
+  },
+  {
+    title: "best desktop wallpapers site",
+    link: "https://wallhaven.cc/"
   }
 ];
